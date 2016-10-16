619d7a23931977a7ba71602d66e2b11505be59d4
Danilo Silveira <dansilveira@live.com>
2016-10-16 16:44:08 -0200

    Diagrama de classes - Cadastro de livros
    Diagramas de classes do crud de livros

--- Projeto ONG/src/fxsistemaong/Controle/TelaCadastroLivrosController.java
+++ Projeto ONG/src/fxsistemaong/Controle/TelaCadastroLivrosController.java
@@ -21,7 +21,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.Tooltip;
 
 /**
  * FXML Controller class
