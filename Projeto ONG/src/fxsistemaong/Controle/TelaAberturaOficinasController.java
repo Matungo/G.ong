8dd35e9b7c092a22a56d13560a70f1bfb3df56a9
Danilo Silveira <dansilveira@live.com>
2016-12-08 05:47:46 -0200

    atualização
    atualização

--- Projeto ONG/src/fxsistemaong/Controle/TelaAberturaOficinasController.java
+++ Projeto ONG/src/fxsistemaong/Controle/TelaAberturaOficinasController.java
@@ -21,8 +21,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TableColumn;
 import javafx.util.StringConverter;
 
 /**
